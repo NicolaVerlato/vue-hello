dfd28dc0c589a05a85e31a09121beb05c479e576
NicolaVerlato <nicola.verlato00@gmail.com>
2022-06-23 13:37:42 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            textMessage: 'Hello',
+            image: 'https://www.pngmart.com/files/12/Greeting-Emoji-PNG-HD.png'
+        }
+    }
+);
